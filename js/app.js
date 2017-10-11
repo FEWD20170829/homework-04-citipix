939ca68860d38402a505ad81f70fb40c49a5be79
kelseycoughlin <khcoughl@gmail.com>
2017-10-10 22:04:12 -0400

    H#4 Changes
    10/10 Changes with help of Kareem

--- js/app.js
+++ js/app.js
@@ -1,62 +1,90 @@
 $(function() {
 
 // listen for click event
+var city;
 
-	$("#submit-btn").click(Update);
+	$("#submit-btn").click(update);
 
-	function Update() {
-		console.log("user clicked on Update button")
+  function update() {
+    console.log("user clicked on update button");
+    city = $("#city-type").val();
+
+    console.log("city");
+
+    if (city === "New York City" || city === "NYC" || city === "New York") {
+
+      console.log("new york was selected");
+      // $("container").attr("class", "nyc");
+    } else {
+      console.log("whatever");
+    }
+     //      $("container").attr("class", "nyc");
+
+
+
+     //    } else if (city === "Los Angeles" || city === "LA" || city === "LAX") {
+
+     //      console.log("los angeles was selected"); 
+     //      $("container").attr("class", "la");
+
+     //    } else if (city === "San Francisco" || city === "SF" || city === "Bay Area") {
+
+      //    console.log("San Francisco was selected");
+      //   $("container").attr("class", "sf");
+
+      // } else if (city === "Austin"|| city === "ATX") {
+
+      //    console.log("Austin was selected");
+      //   $("container").attr("class", "austin");
+
+      // } else if (city === "Sydney" || city === "SYD")
+
+      //    console.log("Sydney was selected");
+      //   $("container").attr("class", "sydney");
+      // }  
 	}
 
 	 // read data typed into city-type
     // store it into a variable "city"
 
-	var city = $("#city-type").val();
-	console.log ('The city entered into city-type field is ', typeof city);
+	// var city = $("#city-type").val();
+	// console.log ('The city entered into city-type field is ', typeof city);
 
-	//display the city-type image
+	// //display the city-type image
 
 	// $("#display").img("city-type");
 
-	 // if New York City is = city say New York City
-    // if Los Angles is = city say Los Angeles
-    // if San Francisco is = city say San Francisco
-    // if Sydney is = city say Sydney
+	//  // if New York City is = city say New York City
+ //    // if Los Angles is = city say Los Angeles
+ //    // if San Francisco is = city say San Francisco
+ //    // if Sydney is = city say Sydney 
 
-	// if (city == New York City) {
- //      console.log("say New York City");
+
+	// if (city === "New York City" || city === "NYC" || city === "New York") {
+
+ //      console.log("new york was selected");
  //      $("container").attr("class", "nyc");
- //    } || else if (city == Los Angeles) {
- //      console.log("say Los Angeles"); 
+
+ //    } else if (city === "Los Angeles" || city === "LA" || city === "LAX") {
+
+ //      console.log("los angeles was selected"); 
  //      $("container").attr("class", "la");
- //    } || else if (city == San Francisco)
-	//    console.log("say San Francisco");
+
+ //    } else if (city === "San Francisco" || city === "SF" || city === "Bay Area") {
+
+	//    console.log("San Francisco was selected");
 	//   $("container").attr("class", "sf");
-	// } || else if (city == Austin)
-	//    console.log("say Austin");
+
+	// } else if (city === "Austin"|| city === "ATX") {
+
+	//    console.log("Austin was selected");
 	//   $("container").attr("class", "austin");
-	// } || else if (city == Sydney)
-	//    console.log("say Sydney");
+
+	// } else if (city === "Sydney" || city === "SYD")
+
+	//    console.log("Sydney was selected");
 	//   $("container").attr("class", "sydney");
 	// }  
-
-
-	if (city == New York City) {
-      console.log("New York City"||"Los Angles"||"San Francisco"||"Austin"||"Sydney");
-      $("container").attr("class", "nyc");
-    } || else if (city == Los Angeles) {
-      console.log("say Los Angeles"); 
-      $("container").attr("class", "la");
-    } || else if (city == San Francisco)
-	   console.log("say San Francisco");
-	  $("container").attr("class", "sf");
-	} || else if (city == Austin)
-	   console.log("say Austin");
-	  $("container").attr("class", "austin");
-	} || else if (city == Sydney)
-	   console.log("say Sydney");
-	  $("container").attr("class", "sydney");
-	}  
 
 
 
