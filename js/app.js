cff9a0a309afdc0c0c738be901babd16c6355582
kelseycoughlin <khcoughl@gmail.com>
2017-10-12 13:57:17 -0400

    H#4
    I think it all looks right but it's just not working. I'm super frustrated!!

--- js/app.js
+++ js/app.js
@@ -1,90 +1,58 @@
 $(function() {
 
-// listen for click event
 var city;
+// listen for click event
 
 	$("#submit-btn").click(update);
 
   function update() {
     console.log("user clicked on update button");
-    city = $("#city-type").val();
+
+      // read data typed into city-type
+    // store it into a variable "city"
+
+    var city = $("#city-type").val();
 
     console.log("city");
+
+    // if New York City is = city say New York City
+    // if Los Angles is = city say Los Angeles
+    // if San Francisco is = city say San Francisco
+    // if Sydney is = city say Sydney 
 
     if (city === "New York City" || city === "NYC" || city === "New York") {
 
       console.log("new york was selected");
-      // $("container").attr("class", "nyc");
-    } else {
-      console.log("whatever");
-    }
-     //      $("container").attr("class", "nyc");
+      $("container").attr("class", "nyc");
 
+    } else (city === "Los Angeles" || city === "LA" || city === "LAX") {
+      
+      console.log("los angeles was selected");
+      $("container").attr("class", "la");
 
+    } else if (city === "San Francisco" || city === "SF" || city === "Bay Area") {
 
-     //    } else if (city === "Los Angeles" || city === "LA" || city === "LAX") {
+      console.log("San Francisco was selected");
+      $("container").attr("class", "sf");
 
-     //      console.log("los angeles was selected"); 
-     //      $("container").attr("class", "la");
+    } else if (city === "Austin"|| city === "ATX") {
 
-     //    } else if (city === "San Francisco" || city === "SF" || city === "Bay Area") {
+      console.log("Austin was selected");
+      $("container").attr("class", "austin");
 
-      //    console.log("San Francisco was selected");
-      //   $("container").attr("class", "sf");
+    } else if (city === "Sydney" || city === "SYD")
 
-      // } else if (city === "Austin"|| city === "ATX") {
-
-      //    console.log("Austin was selected");
-      //   $("container").attr("class", "austin");
-
-      // } else if (city === "Sydney" || city === "SYD")
-
-      //    console.log("Sydney was selected");
-      //   $("container").attr("class", "sydney");
-      // }  
-	}
-
-	 // read data typed into city-type
-    // store it into a variable "city"
-
-	// var city = $("#city-type").val();
-	// console.log ('The city entered into city-type field is ', typeof city);
+      console.log("Sydney was selected");
+      $("container").attr("class", "sydney");
+  }  
 
 	// //display the city-type image
 
-	// $("#display").img("city-type");
-
-	//  // if New York City is = city say New York City
- //    // if Los Angles is = city say Los Angeles
- //    // if San Francisco is = city say San Francisco
- //    // if Sydney is = city say Sydney 
+	$("#display").img("city-type");
 
 
-	// if (city === "New York City" || city === "NYC" || city === "New York") {
 
- //      console.log("new york was selected");
- //      $("container").attr("class", "nyc");
-
- //    } else if (city === "Los Angeles" || city === "LA" || city === "LAX") {
-
- //      console.log("los angeles was selected"); 
- //      $("container").attr("class", "la");
-
- //    } else if (city === "San Francisco" || city === "SF" || city === "Bay Area") {
-
-	//    console.log("San Francisco was selected");
-	//   $("container").attr("class", "sf");
-
-	// } else if (city === "Austin"|| city === "ATX") {
-
-	//    console.log("Austin was selected");
-	//   $("container").attr("class", "austin");
-
-	// } else if (city === "Sydney" || city === "SYD")
-
-	//    console.log("Sydney was selected");
-	//   $("container").attr("class", "sydney");
-	// }  
+	
 
 
 
