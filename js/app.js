d94113730ce632a28ca7bf082546d44e64c4225c
Anastasia Schultis <anastasiaschultis@Anastasias-MacBook-Pro.local>
2017-10-17 20:20:35 -0400

    fixed indentation and added ' to else if statements

--- js/app.js
+++ js/app.js
@@ -3,33 +3,34 @@ $(function() {
 $("#submit-btn").click(changeCity);
 
 function changeCity() {
+
 }
 
 var city = $("#city-type").val();
 
-function changeBackground(changeCity)
+function changeBackground(changeCity) {
 
-if (city === "New York" || city === "NYC" || city === "New York City") {
-  $('body').css('background-image', "url('images/nyc.jpg')");
-}
-
-else if (city === "Los Angeles" || city === "LA") {
-  $('body').css('background-image', "url('images/la.jpg')";
-}
-
- else (city === "San Francisco" || city === "SF") { 
-  $('body').css('background-image',"url('images/sf.jpg');
-}
-
-  else (city === "Austin" || city "ATX") {
-    $('body').css('background-image', "url('images/austin.jpg');
-}
-  else (city === "Sydney" || city "SYD") {
-    $('body').css('background-image', "url(images/sydney.jpg");
-}
-  else {
-    alert("Sorry no city to display");
+  if (city === "New York" || city === "NYC" || city === "New York City") {
+    $('body').css('background-image', "url('images/nyc.jpg')");
   }
 
+  else if (city === "Los Angeles" || city === "LA") {
+    $('body').css('background-image', "url('images/la.jpg')");
+  }
 
+   else if (city === "San Francisco" || city === "SF") { 
+    $('body').css('background-image','url(images/sf.jpg)');
+    console.log('push');
+  }
+
+    else if (city === "Austin" || city === "ATX") {
+      $('body').css('background-image', 'url(images/austin.jpg)');
+  }
+    else if (city === "Sydney" || city === "SYD") {
+      $('body').css('background-image', 'url(images/sydney.jpg)');
+  }
+    else {
+      alert("Sorry no city to display");
+    }
+}
 });
